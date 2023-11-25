9d45f83adb8d379b0c3848cf277ffeec75778d12
George Rowberry <georgewr@stripe.com>
2023-11-25 18:52:19 +0000

    finished

---
+++ src/app/api/abandon/route.ts
@@ -0,0 +1,17 @@
+import Stripe from "stripe";
+
+if (!process.env.STRIPE_SK) {
+  throw "process.env.STRIPE_SK not set";
+}
+
+if (!process.env.READER_ID) {
+  throw "READER_ID not defined";
+}
+const READER_ID = process.env.READER_ID;
+
+const stripe = new Stripe(process.env.STRIPE_SK);
+
+export async function GET(request: Request) {
+  await stripe.terminal.readers.cancelAction(READER_ID);
+  return Response.json({ message: "done" });
+}
---
+++ src/app/api/create_payment/route.ts
@@ -0,0 +1,40 @@
+import Stripe from "stripe";
+
+if (!process.env.STRIPE_SK) {
+  throw "process.env.STRIPE_SK not set";
+}
+
+if (!process.env.READER_ID) {
+  throw "READER_ID not defined";
+}
+const READER_ID = process.env.READER_ID;
+
+const stripe = new Stripe(process.env.STRIPE_SK);
+
+export async function GET(request: Request) {
+  const intent = await stripe.paymentIntents.create({
+    amount: 500,
+    currency: "gbp",
+    payment_method_types: ["card_present"],
+  });
+
+  var attempt = 0;
+  const tries = 3;
+
+  while (attempt < tries) {
+    attempt++;
+    try {
+      const reader = await stripe.terminal.readers.processPaymentIntent(
+        READER_ID,
+        {
+          payment_intent: intent.id,
+        }
+      );
+      Response.json({ message: "Sent to Terminal" });
+      break;
+    } catch (error) {
+      console.log(error);
+      return Response.json(error);
+    }
+  }
+}
